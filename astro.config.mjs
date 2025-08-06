7f90a95ed422b2f23fe14a5f1fc9676c1ee2a95c
mausocuestas <alcatoobr@yahoo.com.br>
2025-08-06 19:48:47 -0300

    Projeto profissa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// Importamos a integração correta que acabamos de instalar
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // E a registramos aqui
+  integrations: [tailwind()]
+});
