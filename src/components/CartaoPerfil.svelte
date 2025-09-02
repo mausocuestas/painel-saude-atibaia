5a0220b6722c4019b3141f4859eab41a406417fd
mausocuestas <alcatoobr@yahoo.com.br>
2025-09-02 15:52:34 -0300

    l3

---
+++ src/components/CartaoPerfil.svelte
@@ -0,0 +1,17 @@
+<div
+  class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center text-center space-y-4 md:flex-row md:space-y-0 md:space-x-4 md:text-left transition-transform hover:scale-105 hover:bg-sky-300"
+>
+  <div class="shrink-0">
+    <img
+      class="h-12 w-12 rounded-full"
+      src="../assets/background"
+      alt="Foto de Ana Silva"
+    />
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black group-hover:text-white">
+      Ana Silva
+    </div>
+    <p class="text-slate-500 group-hover:text-white">Engenheira de Software</p>
+  </div>
+</div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,11 @@ export default {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {primary: "oklch(0.8 0.1081 205.64))", 
+        secondary: "oklch(0.88 0.1312 106.94)"},
+              }
   },
   plugins: [],
 }
+
